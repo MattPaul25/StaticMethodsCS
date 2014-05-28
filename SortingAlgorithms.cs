4a24404444bf9686b8bada2439f0a2da8768769e
Matt Paul <xlsxgeek@gmail.com>
2014-05-28 00:01:17 -0700

    Added String Functions

---
+++ SortingAlgorithms.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tim = DateTime.Now;
+            int[] nums = { 3, 6, 3, 5, 5, 3, 4, 20, 4, 5, 3, 7, 6, 5, 4, 9, 100, 1, 24, 46, 6, 5, 6343, 32, 32, 54, 2, 4, 5, 42, 3, 43, 43, 78, 42, 34, 46 };
+            nums = boolSort(nums);
+            //nums = bubbleSort(nums);
+            System.TimeSpan nTime = DateTime.Now - tim;
+            Console.WriteLine(nTime.ToString());
+            int y = nums.Length;
+            for (int tes = 0; tes < y; tes++)
+            {
+                Console.WriteLine(nums[tes].ToString());
+            }
+
+            Console.ReadLine();
+        }
+        static int[] bubbleSort(int[] nums)
+        {
+            int y = nums.Length;
+            for (int z = 0; z < y; z++)
+            {
+                int x = 0;
+                for (int i = x + 1; i < y; i++)
+                {
+                    if (nums[i] < nums[x])
+                    {
+                        int l = nums[i];
+                        nums[i] = nums[x];
+                        nums[x] = l;
+                    }
+                    x++;
+                }
+            }
+            return nums;
+        }
+        static int[] boolSort(int[] nums)
+        {
+            int nLen = nums.Length;
+            int[] nArray = new int[nLen];
+
+            for (int i = 0; i < nLen; i++)
+            {
+                int x = 0;
+                int count = 0;
+                while (x < nLen)
+                {
+                    if (nums[i] > nums[x] || (nums[i] == nums[x] && x > i))
+                    { count++; }
+                    x++;
+                }
+
+                nArray[count] = nums[i];
+            }
+            return nArray;
+        }
+    }
+}
+
+            for (int i = 0; i < nLen; i++)
+            {
+                int x = 0;
+                int count = 0;
+                while (x < nLen)
+                {
+                    if (nums[i] > nums[x] || (nums[i] == nums[x] && x > i))
+                    { count++; }
+                    x++;
+                }
+
+                nArray[count] = nums[i];
+            }
+            return nArray;
+        }
+
+
+    }
+}
---
+++ StringFunctions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eml = "Matt.Farguson@gmail.com";
+            string emlDomain = RightOf(eml, "@"); //returns gmail.com
+            string emlFirstName = LeftOf(eml, "."); //returns Matt
+            string emlLastName = eml.Substring(Search(eml, "."), Search(eml, "@") - Search(eml, ".")-1); //returns last name       
+            Console.WriteLine("Matt.Farguson@gmail.com's domain is " + emlDomain);
+            Console.WriteLine("Matt.Farguson@gmail.com's first name is " + emlFirstName);
+            Console.WriteLine("The email last name is " + emlLastName);
+            Console.ReadLine();
+        }
+        static string LeftOf(string yourString, string yourMarker)
+        {
+            //method or function that pulls everything left of a unique Marker
+            int anum = 0;
+            int len = yourString.Length;
+            int len2 = yourMarker.Length;
+            string newString = "";
+            do
+            {
+                string temp = yourString.Substring(anum, len2);
+                if (temp == yourMarker)
+                {
+                    return newString;
+                }
+                newString = newString + temp;
+                anum += 1;
+            } while (anum < len);
+            return "";
+        }
+        static string RightOf(string yourString, string yourMarker)
+        {
+            //method or function that pulls everything right of a unique Marker
+            int len = yourString.Length;
+            int len2 = yourMarker.Length;
+            int cnt = 0;
+            for (int i = (len - len2); i > 0; i--)
+            {
+                cnt = cnt + 1;
+                string temp = yourString.Substring(i, len2);
+                if (temp == yourMarker)
+                {
+                    string newString = yourString.Substring(i + len2, cnt - 1);
+                    return newString;
+                }            
+            }
+            return "";
+        }
+        static int Search(string yourString, string yourMarker, int yourInst = 1, bool caseSensitive = true)
+        {
+            //returns the placement of a string in another string
+            int num = 1;
+            int ginst = 1;
+            int mlen = yourMarker.Length;
+            int slen = yourString.Length;
+            string tString = "";
+            try
+            {
+                if (caseSensitive == false)
+                {
+                    yourString = yourString.ToLower();
+                    yourMarker = yourMarker.ToLower();
+                }
+                while (num < slen)
+                {
+                    tString = yourString.Substring(num, mlen);
+
+                    if (tString == yourMarker && ginst == yourInst)
+                    {
+                        return num + 1;
+                    }
+                    else if (tString == yourMarker && yourInst != ginst)
+                    {
+                        ginst += 1;
+                        num += 1;
+                    }
+                    else
+                    {
+                        num += 1;
+                    }
+                }
+                return 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+
+}

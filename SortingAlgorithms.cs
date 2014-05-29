7d3cc09335726bbf55fd6418de526d2e1f6657e7
Matt Fargsuon <xlsxgeek@gmail.com>
2014-05-28 23:17:59 -0700

    Update SortingAlgorithms.cs
    Added Insertion Sort

--- SortingAlgorithms.cs
+++ SortingAlgorithms.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +11,10 @@ namespace SortAlgorithms
         static void Main(string[] args)
         {
             DateTime tim = DateTime.Now;
-            int[] nums = { 3, 6, 3, 5, 5, 3, 4, 20, 4, 5, 3, 7, 6, 5, 4, 9, 100, 1, 24, 46, 6, 5, 6343, 32, 32, 54, 2, 4, 5, 42, 3, 43, 43, 78, 42, 34, 46 };
-            nums = boolSort(nums);
-            //nums = bubbleSort(nums);
+            int[] nums = { 10, 8, 6, 5, 19, 2090, 1, 0, 18, 100, 30, 14, 14, 13, 2, 4, 9000};
+            //nums = boolSort(nums);
+            nums = bubbleSort(nums);
+            //nums = insertionSort(nums);
             System.TimeSpan nTime = DateTime.Now - tim;
             Console.WriteLine(nTime.ToString());
             int y = nums.Length;
@@ -27,7 +28,7 @@ namespace SortAlgorithms
         static int[] bubbleSort(int[] nums)
         {
             int y = nums.Length;
-            for (int z = 0; z < (y-z); z++)
+            for (int z = 0; z < (y - z); z++)
             {
                 int x = 0;
                 for (int i = x + 1; i < y; i++)
@@ -47,6 +48,7 @@ namespace SortAlgorithms
         {
             int nLen = nums.Length;
             int[] nArray = new int[nLen];
+
             for (int i = 0; i < nLen; i++)
             {
                 int x = 0;
@@ -60,6 +62,32 @@ namespace SortAlgorithms
                 nArray[count] = nums[i];
             }
             return nArray;
+        }
+
+        static int[] insertionSort(int[] nums)
+        {
+            int nLen = nums.Length;
+            for (int i = 1; i < nLen; i++)
+            {
+                int y = i;
+                int x = i - 1;
+                while (x >= 0)
+                {
+                    if (nums[y] < nums[x])
+                    {
+                        int plceHolder = nums[x];
+                        nums[x] = nums[y];
+                        nums[y] = plceHolder;
+                        x--;
+                        y--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }     
+            }
+            return nums;
         }
     }
 }

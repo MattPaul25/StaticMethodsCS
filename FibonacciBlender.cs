f87bfe63eca32e02c70fb21bcd016ad014aedb2f
Matt Farguson <xlsxgeek@gmail.com>
2014-06-13 15:27:37 -0700

    Update FibonacciBlender.cs
    added swap and unswap

--- FibonacciBlender.cs
+++ FibonacciBlender.cs
@@ -9,13 +9,23 @@ namespace FibbonacciBlend
     class Program
     {
         //blends the array by positions via indecis created by fibbonacci sequence
-        static int[] origSet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        static int[] origSet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
+                                   14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 
+                                   24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
+                                   34, 35, 36, 37, 38, 39, 40 };
         static void Main(string[] args)
         {
-            int[] swappedSet = Blender(origSet, 8);
+            int[] swappedSet;
+            //swappedSet = FibbyBlender(origSet, 9);
+            swappedSet = Swap(origSet, 2);
+            swappedSet = Swap(swappedSet, 2); //this reverses the order back to normal
+            for (int j = 0; j < origSet.Length; j++)
+            {
+                Console.WriteLine(swappedSet[j].ToString());
+            }
+            Console.ReadLine();
         }
-
-        private static int[] Blender(int[] origSet, int finNum)
+        private static int[] FibbyBlender(int[] origSet, int finNum)
         {
             int holder;
             int[] newSet = origSet;
@@ -35,11 +45,31 @@ namespace FibbonacciBlend
                 {
                     newSet[i] = origSet[i];
                 }
-
             }
             return newSet;
         }
-
+        private static int[] FibbyReOrder(int[] origSet, int finNum)
+        {
+            int holder;
+            int[] newSet = origSet;
+            int[] blendedSet = new int[origSet.Length];
+            for (int i = finNum; i >= finNum; i++)
+            {
+                int swapNum = fibby(i);
+                blendedSet[i] = swapNum;
+                if (blendedSet[i] != 0)
+                {
+                    holder = newSet[i];
+                    newSet[i] = newSet[blendedSet[i]];
+                    newSet[blendedSet[i]] = holder;
+                }
+                else if (newSet[i] == 0)
+                {
+                    newSet[i] = origSet[i];
+                }
+            }
+            return newSet;
+        }
         private static int fibby(int i)
         {
             int cnt = 0;
@@ -54,10 +84,24 @@ namespace FibbonacciBlend
                 lead = holder + behind;
                 cnt++;
             }
-
             return lead;
-
         }
-
+        private static int[] Swap(int[]origSet, int sNum)
+        {
+            int[] NewSet = origSet;
+            for (int i = 0; i < NewSet.Length; i = i + sNum + 1 )
+            {
+                int holder = NewSet[i];
+                int j = i + 1;
+                try
+                {
+                    NewSet[i] = NewSet[j];
+                    NewSet[j] = holder;
+                }
+                catch { }
+            }
+            return NewSet;        
+        }
     }
+
 }

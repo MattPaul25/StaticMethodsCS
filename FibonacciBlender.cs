f12a8c9961f8af5906b11a29b8edc1dc5c6848f2
Matt Farguson <xlsxgeek@gmail.com>
2014-06-26 01:15:06 -0700

    Update FibonacciBlender.cs
    update on pull method - brought back in to fix fibby blender issue (reorder gets out of range) - kill the second arg in fibby blender / reorder

--- FibonacciBlender.cs
+++ FibonacciBlender.cs
@@ -9,53 +9,55 @@ namespace FibbonacciBlend
     class Program
     {
         //blends the array by positions via indecis created by fibbonacci sequence
-        public static string MyString = "Just stop would you, you need to tip for GOD SAKES";              
+
+        public static string MyString = "This is a typical String. Its got letters and Numbers (like 4), along with punctation! ";
+
+
         static void Main(string[] args)
         {
-
-            string thisString = Push(MyString);
-            Console.WriteLine(thisString);
-            Console.ReadLine();
-            Console.WriteLine(Pull(thisString));
-            Console.ReadLine();
-
-            string aString = outIn(MyString);
-            Console.WriteLine(aString);
-            Console.ReadLine();
-            aString = inOut(aString);
-            Console.WriteLine(aString);
-            Console.ReadLine();
+            //mucks it up
 
             Console.WriteLine("Please Write a String");
+
             string ConsoleString = Console.ReadLine();
             Console.WriteLine(ConsoleString);
             Console.WriteLine();
-            Console.WriteLine("Lets merge the string");
-            ConsoleString = mergeText(ConsoleString);
-            Console.WriteLine("After Merging: " + ConsoleString);
-            Console.ReadLine();
-            ConsoleString = FibbyBlender(ConsoleString);
+            //Console.WriteLine("Lets Pull the string over");
+            //ConsoleString = Pull(ConsoleString);
+            //Console.WriteLine(ConsoleString);
+            //Console.ReadLine();
+            //Console.WriteLine("Lets fix it");
+            //ConsoleString = Push(ConsoleString);
+            //Console.WriteLine(ConsoleString);
+            //Console.ReadLine();
+            //Console.WriteLine("Lets merge the string");
+            //ConsoleString = mergeText(ConsoleString);
+            //Console.WriteLine("After Merging: " + ConsoleString);
+            //Console.ReadLine();
+
+            ConsoleString = FibbyBlender(ConsoleString, 10);
             Console.WriteLine("After FibBlend: " + ConsoleString);
             Console.ReadLine();
-            ConsoleString = Swap(ConsoleString, 1);
-            Console.WriteLine("After Swap: " + ConsoleString);
-            Console.ReadLine();
-            Console.WriteLine("The procedures to clean the file up start after enter...");
-            Console.ReadLine();
-            ////puts it back together
-            ConsoleString = Swap(ConsoleString, 1);
-            Console.WriteLine("After Re-swap: " + ConsoleString);
-            Console.ReadLine();
-            ConsoleString = FibbyReOrder(ConsoleString); //must be higher than fibby blender order of magnitude
+            //ConsoleString = Swap(ConsoleString, 1);
+            //Console.WriteLine("After Swap: " + ConsoleString);
+            //Console.ReadLine();
+            //Console.WriteLine("The procedures to clean the file up start after enter...");
+            //Console.ReadLine();
+            //////puts it back together
+            //ConsoleString = Swap(ConsoleString, 1);
+            //Console.WriteLine("After Re-swap: " + ConsoleString);
+            //Console.ReadLine();
+            ConsoleString = FibbyReOrder(ConsoleString, 10); //must be higher than fibby blender order of magnitude
             Console.WriteLine("After Fib-ReOrder: " + ConsoleString);
             Console.ReadLine();
-            ConsoleString = unMergeText(ConsoleString);
-            Console.WriteLine("After UnMerging the String: " + ConsoleString);
-            Console.ReadLine();
+            //ConsoleString = unMergeText(ConsoleString);
+            //Console.WriteLine("After UnMerging the String: " + ConsoleString);
+            //Console.ReadLine();
         }
+
         private static string mergeText(string someText)
         {
-            int strLength = someText.Length - 1 ;
+            int strLength = someText.Length - 1;
             for (int i = 0; i <= strLength; i = i + 2)
             {
                 string myChar = someText.Substring(strLength, 1);
@@ -72,9 +74,9 @@ namespace FibbonacciBlend
             int strLength = someText.Length - 1;
             int cntD = strLength;
             int cntU = 0;
-            for (int i = 0; i <= strLength; i = i +2)
+            for (int i = 0; i <= strLength; i = i + 2)
             {
-                string myChar = someText.Substring(i,1);
+                string myChar = someText.Substring(i, 1);
                 int x = i + 1;
                 try
                 {
@@ -93,11 +95,11 @@ namespace FibbonacciBlend
         {
             string newString = "";
             int i;
-            for (i = 0; i < stringArr.Length; i++) 
+            for (i = 0; i < stringArr.Length; i++)
             {
                 newString = newString + stringArr[i];
             }
-            return newString; 
+            return newString;
         }
         static string Left(string yourString, int PlaceMent)
         {
@@ -107,36 +109,45 @@ namespace FibbonacciBlend
         {
             return yourString.Substring(PlaceMent, yourString.Length - PlaceMent);
         }
-        private static string FibbyBlender(string someText)
+        private static string FibbyBlender(string someText, int finNum)
         {
             string holder;
             string[] origSet = SimpleSplit(someText);
             string[] newSet = origSet;
             int[] blendedSet = new int[newSet.Length];
-            for (int i = 0; i < someText.Length; i++)
+            for (int i = 0; i < finNum; i++)
             {
+                try{
                 int swapChar = fibby(i, someText);
                 blendedSet[i] = swapChar;
-                if (blendedSet[i] != 0)
-                {
-                    holder = newSet[i];
-                    newSet[i] = newSet[blendedSet[i]];
-                    newSet[blendedSet[i]] = holder;
+
+
+                    if (blendedSet[i] != 0)
+                    {
+                        holder = newSet[i];
+                        newSet[i] = newSet[blendedSet[i]];
+                        newSet[blendedSet[i]] = holder;
+                    }
+
+
+                    else if (newSet[i] == "")
+                    {
+                        newSet[i] = origSet[i];
+                    }
                 }
-                else if (newSet[i] == "")
-                {
-                    newSet[i] = origSet[i];
-                }
+                catch { }
+
             }
+
             return convertArray(newSet);
         }
-        private static string[] SimpleSplit(string aString) 
+        private static string[] SimpleSplit(string aString)
         {
             string[] newArr = new string[aString.Length];
 
-            for (int i = 0; i < aString.Length; i++)            
+            for (int i = 0; i < aString.Length; i++)
             {
-                newArr[i] = aString.Substring(i, 1);            
+                newArr[i] = aString.Substring(i, 1);
             }
             return newArr;
         }
@@ -156,13 +167,13 @@ namespace FibbonacciBlend
             }
             return lead;
         }
-        private static string FibbyReOrder(string someText)
+        private static string FibbyReOrder(string someText, int finNum)
         {
             string holder;
             string[] origSet = SimpleSplit(someText);
             string[] newSet = origSet;
             int[] blendedSet = new int[newSet.Length];
-            for (int i = (someText.Length - 1); i >= 0; i--)
+            for (int i = finNum; i >= 0; i--)
             {
                 int swapNum = fibby(i, someText);
                 blendedSet[i] = swapNum;
@@ -194,11 +205,11 @@ namespace FibbonacciBlend
                 cnt++;
             }
             return behind;
-        }        
+        }
         private static string Swap(string someText, int sNum)
         {
             string[] NewSet = SimpleSplit(someText);
-            for (int i = 0; i < NewSet.Length; i = i + sNum + 1 )
+            for (int i = 0; i < NewSet.Length; i = i + sNum + 1)
             {
                 string holder = NewSet[i];
                 int j = i + 1;
@@ -209,100 +220,25 @@ namespace FibbonacciBlend
                 }
                 catch { }
             }
-            return convertArray(NewSet);        
-        }
-        private static string outIn(string someText)
-        {
-            int step;
-            int x ;
-            int i = 0;
-            int len = someText.Length;
-            if (len % 2 == 1)
-            {
-                x = (len + 1) / 2;
-                while (i < x/2)
-                {
-                    string lMove = someText.Substring(0, 1);
-                    string rMove = someText.Substring(len - 1, 1);
-                    someText = someText.Insert(len - x, rMove);
-                    someText = someText.Substring(0, len);
-                    someText = someText.Insert(x, lMove);
-                    someText = someText.Substring(1, len);
-                    i++;
-                }
-            }
-            else
-            {
-                x = len / 2;
-                step = 1;
-                while (i < x/2)
-                {
-                    string lMove = someText.Substring(0, 1);
-                    string rMove = someText.Substring(len - 1, 1);
-                    someText = someText.Insert(x - step, rMove);
-                    someText = someText.Substring(0, len);
-                    someText = someText.Insert(x + step, lMove);
-                    someText = someText.Substring(1, len);
-                    i++;
-                }
-            }
-            return someText;
-        }
-        private static string inOut(string someText)
-        {
-            int step;
-            int x;
-            int i = 0;
-            int len = someText.Length;
-            if (len % 2 == 1)
-            {
-                x = (len + 1) / 2;
-                step = 0;
-                while (i < x/2)
-                {
-                    string lMove = someText.Substring(x - 1, 1);
-                    someText = someText.Remove(x - 1, 1);
-                    someText = someText.Insert(0, lMove);
-                    string rMove = someText.Substring(x - 1, 1); //
-                    someText = someText.Remove(x - 1, 1);
-                    someText = someText.Insert(len - 1, rMove);
-                    i++;
-                }
-            }
-            else
-            {
-                x = len / 2;
-                step = 1;
-                while (i < x/2 )
-                {
-                    string lMove = someText.Substring(x, 1);
-                    someText = someText.Remove(x, 1);
-                    someText = someText.Insert(0, lMove);
-                    string rMove = someText.Substring(x - step, 1);
-                    someText = someText.Remove(x - step, 1);
-                    someText = someText.Insert(len - 1, rMove);
-                    i++;
-                }
-            }
-            return someText;
+            return convertArray(NewSet);
         }
         private static string Push(string someText)
         {
 
-            int num = someText.Length % 2 == 1 ? (someText.Length + 1) /2 : someText.Length / 2;
+            int num = someText.Length % 2 == 1 ? (someText.Length + 1) / 2 : someText.Length / 2;
             int i = 0;
-            while (i < num){
+            while (i < num)
+            {
                 string myChar = someText.Substring(0, 1);
                 someText = someText.Substring(1, someText.Length - 1) + myChar;
                 i++;
-            }          
+            }
             return someText;
         }
         private static string Pull(string someText)
         {
-
             int num = someText.Length % 2 == 1 ? (someText.Length + 1) / 2 : someText.Length / 2;
-            int i = someText.Length - 1;
+            int i = someText.Length % 2 == 1 ? (someText.Length) : someText.Length - 1; 
             while (i >= num)
             {
                 string myChar = someText.Substring(someText.Length - 1, 1);
